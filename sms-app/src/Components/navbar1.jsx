196218147b8bceb7d81fef0e17c468af7fded0ea
Madhu M Pandurangi <madhumpandurangi@gmail.com>
2020-02-20 17:58:07 +0530

    updated navbar1.jsx

--- sms-app/src/Components/navbar1.jsx
+++ sms-app/src/Components/navbar1.jsx
@@ -4,7 +4,7 @@ class NavBar1 extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <div className="navbar-brand"><a className="text-success" href="#">Home</a></div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -12,9 +12,15 @@ class NavBar1 extends Component {
                 <div className="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Student <span className="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Teacher <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About Us <span className="sr-only">(current)</span></a>
+                        </li>
+                        {/* <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
                         </li>
                         <li className="nav-item">
@@ -27,7 +33,7 @@ class NavBar1 extends Component {
                                 <a className="dropdown-item" href="#">Another action</a>
                                 <a className="dropdown-item" href="#">Something else here</a>
                             </div>
-                        </li>
+                        </li> */}
                     </ul>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
