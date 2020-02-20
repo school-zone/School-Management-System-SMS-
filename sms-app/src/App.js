60d6b5a19f149c1609cb9d67ed54ccf7e38e6253
Madhu M Pandurangi <madhumpandurangi@gmail.com>
2020-02-20 11:17:56 +0530

    Created a first demo page of this app and publish it to GitHub Pages

--- sms-app/src/App.js
+++ sms-app/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar1 from './Components/navbar1';
+import HelloWorld from './Components/helloWorld';
+import Content from './Components/firstPageContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <NavBar1 />
+      <main role="main">
+
+        {/* <!-- Main jumbotron for a primary marketing message or call to action --> */}
+        <HelloWorld />
+
+        {/* <!-- /container --> */}
+        <Content />
+
+        <footer class="container">
+          <p>&copy; A@M Software Solutions 2020</p>
+        </footer>
+      </main>
+    </React.Fragment>
   );
 }
 
